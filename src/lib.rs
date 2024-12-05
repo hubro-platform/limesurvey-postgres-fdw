9559b7f1ec20b66e1d52c59bbfdf94cb1743659a
Mirek Muzny <mmuzny@gmail.com>
2024-12-05 10:50:47 +0100

    Rename and enhance FDW for FHIR data integration
    Renamed the FDW from "ExampleFdw" to "FhirFdw" and updated its functionalities to support FHIR data integration with PostgreSQL. This includes adding HTTP request handling for FHIR resources, pagination features, and data conversion improvements for different PostgreSQL data types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,30 @@
 #[allow(warnings)]
 mod bindings;
-use serde_json::Value as JsonValue;
+use serde_json::{json, Map as JsonMap, Value as JsonValue};
 
 use bindings::{
     exports::supabase::wrappers::routines::Guest,
     supabase::wrappers::{
         http, time,
-        types::{Cell, Context, FdwError, FdwResult, OptionsType, Row, TypeOid},
+        types::{Cell, Column, Context, FdwError, FdwResult, OptionsType, Row, TypeOid, Value},
         utils,
     },
 };
 
 #[derive(Debug, Default)]
-struct ExampleFdw {
+struct FhirFdw {
     base_url: String,
+    url: Option<String>,
+    headers: Vec<(String, String)>,
+    object: String,
     src_rows: Vec<JsonValue>,
     src_idx: usize,
 }
 
 // pointer for the static FDW instance
-static mut INSTANCE: *mut ExampleFdw = std::ptr::null_mut::<ExampleFdw>();
+static mut INSTANCE: *mut FhirFdw = std::ptr::null_mut::<FhirFdw>();
 
-impl ExampleFdw {
+impl FhirFdw {
     // initialise FDW instance
     fn init_instance() {
         let instance = Self::default();
@@ -33,9 +36,151 @@ impl ExampleFdw {
     fn this_mut() -> &'static mut Self {
         unsafe { &mut (*INSTANCE) }
     }
+
+    fn can_pushdown_id(&self) -> bool {
+        self.object.starts_with("Observation")
+    }
+
+    fn page_size(&self) -> usize {
+        match self.object.as_str() {
+            "transactions" => 30,
+            "adjustments" => 50,
+            _ => 200,
+        }
+    }
+
+    fn src_to_cell(&self, src_row: &JsonValue, tgt_col: &Column) -> Result<Option<Cell>, FdwError> {
+        let tgt_col_name = tgt_col.name();
+
+        if &tgt_col_name == "attrs" {
+            return Ok(Some(Cell::Json(src_row.to_string())));
+        }
+
+        let src = src_row
+            .as_object()
+            .and_then(|v| v.get(&tgt_col_name))
+            .ok_or(format!("source column '{}' not found", tgt_col_name))?;
+
+        let cell = match tgt_col.type_oid() {
+            TypeOid::Bool => src.as_bool().map(Cell::Bool),
+            TypeOid::I8 => src.as_i64().map(|v| Cell::I8(v as i8)),
+            TypeOid::I16 => src.as_i64().map(|v| Cell::I16(v as i16)),
+            TypeOid::F32 => src.as_f64().map(|v| Cell::F32(v as f32)),
+            TypeOid::I32 => src.as_i64().map(|v| Cell::I32(v as i32)),
+            TypeOid::F64 => src.as_f64().map(Cell::F64),
+            TypeOid::I64 => src.as_i64().map(Cell::I64),
+            TypeOid::Numeric => src.as_f64().map(Cell::Numeric),
+            TypeOid::String => src.as_str().map(|v| Cell::String(v.to_owned())),
+            TypeOid::Date => {
+                if let Some(s) = src.as_str() {
+                    let ts = time::parse_from_rfc3339(s)?;
+                    Some(Cell::Date(ts / 1_000_000))
+                } else {
+                    None
+                }
+            }
+            TypeOid::Timestamp => {
+                if let Some(s) = src.as_str() {
+                    let ts = time::parse_from_rfc3339(s)?;
+                    Some(Cell::Timestamp(ts))
+                } else {
+                    None
+                }
+            }
+            TypeOid::Timestamptz => {
+                if let Some(s) = src.as_str() {
+                    let ts = time::parse_from_rfc3339(s)?;
+                    Some(Cell::Timestamptz(ts))
+                } else {
+                    None
+                }
+            }
+            TypeOid::Json => src.as_object().map(|_| Cell::Json(src.to_string())),
+        };
+
+        Ok(cell)
+    }
+
+    fn make_request(&mut self, ctx: &Context) -> FdwResult {
+        let quals = ctx.get_quals();
+
+        let url = if let Some(ref url) = self.url {
+            url.clone()
+        } else {
+            let object = quals
+                .iter()
+                .find(|q| q.field() == "id")
+                .and_then(|id| {
+                    if !self.can_pushdown_id() {
+                        return None;
+                    }
+
+                    // push down id filter
+                    match id.value() {
+                        Value::Cell(Cell::String(s)) => Some(format!("{}/{}", self.object, s)),
+                        _ => None,
+                    }
+                })
+                .unwrap_or_else(|| self.object.clone());
+            format!("{}/{}?per_page={}", self.base_url, object, self.page_size())
+        };
+        let req = http::Request {
+            method: http::Method::Get,
+            url,
+            headers: self.headers.clone(),
+            body: String::default(),
+        };
+        let resp = http::get(&req)?;
+        let resp_json: JsonValue = serde_json::from_str(&resp.body).map_err(|e| e.to_string())?;
+
+        // if the 404 is caused by no object found, we shouldn't take it as an error
+        if resp.status_code == 404 && resp_json.pointer("/error/code") == Some(&json!("not_found"))
+        {
+            self.src_rows = Vec::default();
+            self.src_idx = 0;
+            self.url = None;
+            return Ok(());
+        }
+
+        http::error_for_status(&resp).map_err(|err| format!("{}: {}", err, resp.body))?;
+
+        // save source rows
+        self.src_rows = resp_json
+            .as_object()
+            .and_then(|v| v.get("data"))
+            .and_then(|v| {
+                // convert a single object response to an array
+                if v.is_object() {
+                    Some(vec![v.to_owned()])
+                } else {
+                    v.as_array().cloned()
+                }
+            })
+            .ok_or("cannot get query result data")?;
+        self.src_idx = 0;
+
+        // deal with pagination to save next page url
+        let pagination = resp_json
+            .pointer("/meta/pagination")
+            .and_then(|v| v.as_object());
+        let has_more = pagination
+            .and_then(|v| v.get("has_more"))
+            .and_then(|v| v.as_bool())
+            .unwrap_or_default();
+        self.url = if has_more {
+            pagination
+                .and_then(|v| v.get("next"))
+                .and_then(|v| v.as_str())
+                .map(|v| v.to_owned())
+        } else {
+            None
+        };
+
+        Ok(())
+    }
 }
 
-impl Guest for ExampleFdw {
+impl Guest for FhirFdw {
     fn host_version_requirement() -> String {
         // semver expression for Wasm FDW host version requirement
         // ref: https://docs.rs/semver/latest/semver/enum.Op.html
@@ -47,74 +192,55 @@ impl Guest for ExampleFdw {
         let this = Self::this_mut();
 
         let opts = ctx.get_options(OptionsType::Server);
-        this.base_url = opts.require_or("api_url", "https://api.github.com");
+        this.base_url = opts.require_or("api_url", "https://hapi.fhir.org/baseR4");
+        let api_key = match opts.get("api_key") {
+            Some(key) => key,
+            None => {
+                let key_id = opts.require("api_key_id")?;
+                utils::get_vault_secret(&key_id).unwrap_or_default()
+            }
+        };
+
+        this.headers
+            .push(("Content-Type".to_owned(), "application/json".to_string()));
+        // this.headers
+        //     .push(("authorization".to_owned(), format!("Bearer {}", api_key)));
 
         Ok(())
     }
 
     fn begin_scan(ctx: &Context) -> FdwResult {
         let this = Self::this_mut();
-
         let opts = ctx.get_options(OptionsType::Table);
-        let object = opts.require("object")?;
-        let url = format!("{}/{}", this.base_url, object);
+        this.object = opts.require("object")?;
 
-        let headers: Vec<(String, String)> =
-            vec![("user-agent".to_owned(), "Example FDW".to_owned())];
-
-        let req = http::Request {
-            method: http::Method::Get,
-            url,
-            headers,
-            body: String::default(),
-        };
-        let resp = http::get(&req)?;
-        let resp_json: JsonValue = serde_json::from_str(&resp.body).map_err(|e| e.to_string())?;
-
-        this.src_rows = resp_json
-            .as_array()
-            .map(|v| v.to_owned())
-            .expect("response should be a JSON array");
-
-        utils::report_info(&format!("We got response array length: {}", this.src_rows.len()));
+        this.url = None;
+        this.make_request(ctx)?;
 
         Ok(())
     }
 
+
+
     fn iter_scan(ctx: &Context, row: &Row) -> Result<Option<u32>, FdwError> {
         let this = Self::this_mut();
 
+        // if all local rows are consumed
         if this.src_idx >= this.src_rows.len() {
-            return Ok(None);
+
+            // if no more pages, stop the iter scan
+            if this.url.is_none() {
+                return Ok(None);
+            }
+
+            // otherwise, make another call to get next page data
+            this.make_request(ctx)?;
         }
 
+        // convert FHIR row to Postgres row
         let src_row = &this.src_rows[this.src_idx];
         for tgt_col in ctx.get_columns() {
-            let tgt_col_name = tgt_col.name();
-            let src = src_row
-                .as_object()
-                .and_then(|v| v.get(&tgt_col_name))
-                .ok_or(format!("source column '{}' not found", tgt_col_name))?;
-            let cell = match tgt_col.type_oid() {
-                TypeOid::Bool => src.as_bool().map(Cell::Bool),
-                TypeOid::String => src.as_str().map(|v| Cell::String(v.to_owned())),
-                TypeOid::Timestamp => {
-                    if let Some(s) = src.as_str() {
-                        let ts = time::parse_from_rfc3339(s)?;
-                        Some(Cell::Timestamp(ts))
-                    } else {
-                        None
-                    }
-                }
-                TypeOid::Json => src.as_object().map(|_| Cell::Json(src.to_string())),
-                _ => {
-                    return Err(format!(
-                        "column {} data type is not supported",
-                        tgt_col_name
-                    ));
-                }
-            };
-
+            let cell = this.src_to_cell(src_row, &tgt_col)?;
             row.push(cell.as_ref());
         }
 
@@ -123,8 +249,10 @@ impl Guest for ExampleFdw {
         Ok(Some(0))
     }
 
-    fn re_scan(_ctx: &Context) -> FdwResult {
-        Err("re_scan on foreign table is not supported".to_owned())
+    fn re_scan(ctx: &Context) -> FdwResult {
+        let this = Self::this_mut();
+        this.url = None;
+        this.make_request(ctx)
     }
 
     fn end_scan(_ctx: &Context) -> FdwResult {
@@ -154,4 +282,4 @@ impl Guest for ExampleFdw {
     }
 }
 
-bindings::export!(ExampleFdw with_types_in bindings);
+bindings::export!(FhirFdw with_types_in bindings);
